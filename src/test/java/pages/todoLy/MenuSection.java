9bd614da67c254c4fee16030e383dfe9db876ce3
eynar-pari-nybblegroup <eynar.pari@nybblegroup.com>
2022-10-21 11:16:22 -0400

    build structure

---
+++ src/test/java/pages/todoLy/MenuSection.java
@@ -0,0 +1,9 @@
+package pages.todoLy;
+
+import controlSelenium.Button;
+import org.openqa.selenium.By;
+
+public class MenuSection {
+
+    public Button logoutButton = new Button(By.xpath("//a[text()='Logout']"));
+}
